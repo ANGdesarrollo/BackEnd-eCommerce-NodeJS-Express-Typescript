04444610cfee13100cfec695277ace6ff5809c49
Alexis Graff <alexisgraff123@gmail.com>
2023-03-29 22:30:04 -0300

    test vite

--- public/src/App.tsx
+++ public/src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
-import viteLogo from '../../../../../../../../vite.svg';
 import './App.css';
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
@@ -17,7 +16,7 @@ function App() {
     <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={reactLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
