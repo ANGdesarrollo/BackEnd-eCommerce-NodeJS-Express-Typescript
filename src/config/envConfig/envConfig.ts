ca10b6f0b04028faa0401b6d78d393091f1f34b7
Alexis Graff <alexisgraff123@gmail.com>
2023-04-21 12:27:20 -0300

    cambio de .env a la base del repo

--- src/config/envConfig/envConfig.ts
+++ src/config/envConfig/envConfig.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 import { type IconfigIterface } from './envConfigInterface';
-dotenv.config({path: './src/.env'});
+dotenv.config();
 
 if (!process.env.NODE_ENV) {
   throw new Error('Enviroment not properly set. NODE_ENV not set');
